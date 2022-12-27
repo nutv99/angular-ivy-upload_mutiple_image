b1aeceab95c3dbe137043ce7910244aef325d3ed
nutv99 <nutv99@gmail.com>
2022-12-27 09:40:03 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-upload_mutiple_image
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-mcviqu)
---
+++ src/app/app.component.ts
@@ -0,0 +1,41 @@
+import { Component, VERSION, ViewChild, ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  name = 'Angular ' + VERSION.major;
+  url?: Blob;
+  url2: string | ArrayBuffer;
+  showFileSelect: boolean = false;
+
+  @ViewChild('fileImg') myNameElem: ElementRef;
+  @ViewChild('imgContainner') imgContainer99: ElementRef;
+  @ViewChild('fileSelect') fileSelect: ElementRef;
+
+  AddSelectFile() {
+    this.fileSelect.nativeElement.click();
+  }
+
+  onSelectFile(event) {
+    if (event.target.files && event.target.files[0]) {
+      var reader = new FileReader();
+
+      // alert(event.target.files.length)   ;
+      // for (let i = 0; i <= event.target.files.length - 1; i++) {
+      reader.readAsDataURL(event.target.files[0]);
+      reader.onload = (event) => {
+        //var img = new Image();
+        this.url2 = event.target.result;
+        console.log(this.url2);
+        this.myNameElem.nativeElement.arrayBuffer = this.url2;
+        this.imgContainer99.nativeElement.innerHTML +=
+          '<img src="' + this.url2 + '" width=100>';
+        //console.log(img.width);
+      };
+      // } // for
+    }
+  }
+}
